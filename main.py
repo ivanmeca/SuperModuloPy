599fcc3c34790a6b088f0ba6ea29029e871544fa
Ivan Mascarenhas <ivan.mascarenhas@hp.com>
2024-02-19 19:47:53 -0300

    Primeiro exercicio

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-msg = 'Teste'
+import re
 
-print('Hello World!')
-print(type(msg))
+print("Olá mundo!")
+
+print(f'o numero informado foi {input('Digite um valor: ')}')
+
+nome = input('Digite um valor inteiro:')
+idade = input('Digite sua idade:')
+cpf = input('Digite seu CPF:')
+cpfMask = re.compile(r'\d{3}\.\d{3}\.\d{3}-\d{2}')
+print(cpfMask.match(cpf))
+print(f'Olá {nome}\nSeus dados cadastrados foram: Idade = {idade} e CPF = {cpf}')
+
+
+
