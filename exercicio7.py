c5efee40f95d388c7e22a53772f4d22a7d67ab49
Ivan Mascarenhas <ivan.mascarenhas@hp.com>
2024-02-26 19:51:57 -0300

    Exercicio while

--- exercicio7.py
+++ exercicio7.py
@@ -1,9 +1,10 @@
 import random
+import sys
 
 
-def NewPrompt(prompt):
+def new_prompt(prompt):
     input('Pressione ENTER para continuar')
-    for x in range(100):
+    for _ in range(100):
         print('\n')
     print(prompt)
 
@@ -17,22 +18,23 @@ def NewPrompt(prompt):
 #     x += 1
 
 # Exercicio jogo de adivinhação
-NewPrompt('Exercicio - jogo')
-numeroAlvo = int(random.uniform(0, 100))
-n_tentativas = 0
-tentativa = 0
-limites = [0,100]
-while n_tentativas <= 10:
-    tentativa = int(input(f'Tentativa {n_tentativas}, o numero está entre {limites[0]} e {limites[1]}. Digite um numero: '))
-    if tentativa == numeroAlvo:
+new_prompt('Exercicio - jogo')
+NumeroAlvo = int(random.uniform(0, 100))
+N_Tentativas = 0
+Tentativa = 0
+Limites = [0, 100]
+while N_Tentativas <= 10:
+    Tentativa = int(input(f'Tentativa {N_Tentativas}, o numero está entre {Limites[0]} e '
+                          f'{Limites[1]}. Digite um numero: '))
+    if Tentativa == NumeroAlvo:
         print('Parabens, você acertou!!')
-        exit(0)
-    if tentativa > numeroAlvo:
+        sys.exit(0)
+    if Tentativa > NumeroAlvo:
         print('O número é menor')
-        limites[1] = tentativa
+        Limites[1] = Tentativa
     else:
         print('O número é maior')
-        limites[0] = tentativa
-    n_tentativas += 1
+        Limites[0] = Tentativa
+    N_Tentativas += 1
 
-print(f'O numero era {numeroAlvo}')
+print(f'Limite de tentativas, o numero era {NumeroAlvo}')
