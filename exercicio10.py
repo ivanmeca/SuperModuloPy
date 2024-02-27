80f51dc73c706642e6556555ec6e33faed45a62c
Ivan Mascarenhas <ivan.mascarenhas@hp.com>
2024-02-26 21:49:50 -0300

    Exercicio while

--- exercicio10.py
+++ exercicio10.py
@@ -26,7 +26,6 @@ for x in range(10):
     valor = int(input(f'Tentativa {x}. Digite um numero: '))
     if not valor % 6:
         Soma += valor
-
     if Soma >= 30:
         break
 print(f'Final de jogo, a soma foi {Soma}')
